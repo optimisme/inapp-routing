e8628d7a67498744d2bd0a09bf91467ea1436f08
Albert <optimisme@gmail.com>
2020-05-13 14:39:41 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # html5-routing
 Micro library to add HTML5 routing capabilities to any website
+
+## Example
+https://optimisme.github.io/html5-routing/.
+
+## Features
+
+It does not use '#', making it compatible with intra navigation sites and plugins
+
+It uses the navigation history from the browser where each route is defined by '/sectionroute'
+
+Users can navigate with 'back' and 'forward' buttons
+
+Programmers can use 'a' tags to change to another screen: '<a href="/sectionroute">' 
+
+Or, programmers can change to another screen from javascript with 'routing.changeTo('/list')'
+
+## How to use it
+
+Add this library with:
+<script src='/routing.js'></script>
+
+This script will add one custom element named 'route-screen' that you can use like:
+<route-screen id='home'>Home screen contents</route-screen>
+
+Each 'route-screen' element contains the html source of one navigation screen of the application
+
+The library automatically handles the window.history states and sets the 'display' property of each 'route-screen' element to 'none' if the route doesn't match
+
+The id of each 'root-screen' element matches its URL when shown
+
+By default, the '/' route will show the first 'root-screen' defined (use id="home" as the first 'route-screen' is recommended)
+
+Finally, 'route-screen' elements can define two functions:
+
+<route-screen id='home' onshow='console.log(this.args)'>Home screen contents</route-screen>
+'onshow' will be called after that 'route' is shown (its 'style.display' property will be set to its default value)
+
+Programmers can get the parameters of the URL with 'this.args', for example when the next link is clicked:
+<a href='/detail/3/A'>Product 3 with config A</a>
+
+- Navigator will set the URL to '/detail/3/A'
+- Section <route-screen id='detail' onshow='loadDetail(this.args)'> will be shown
+- loadDetail function will be called with ['3', 'A'] parameters
+
+<route-screen id='home' onshow='console.log(this.args)'>Home screen contents</route-screen>
+'onhide' will be called before that 'route' is hidden (its 'style.display' property will be set to 'none')
+
+These functions can return a 'Promise' and the library will wait until the call is resolved (see the animated example).
---
+++ routing.js
@@ -0,0 +1,119 @@
+class ObjRouteScreen extends HTMLElement {
+    constructor () { super() }
+    static get name () { return 'route-screen' }
+}
+
+if (customElements.get(ObjRouteScreen.name) === undefined) {
+    customElements.define(ObjRouteScreen.name, ObjRouteScreen)
+}
+
+class ObjRouting {
+
+    constructor () {
+        this.defaultRoute = '/home'
+        this.route = this.defaultRoute
+        this.args = []
+        this.handler = this.init.bind(this)
+
+        window.addEventListener('load', () => { this.init() })
+        window.addEventListener('popstate', (e) => { this.changeTo(e.state.html, true); return false })
+        document.addEventListener('click',  (e) => {
+            let element = e.target || e.srcElement
+            if (element.tagName == 'A') {
+              e.preventDefault()
+              e.stopPropagation()
+              this.changeTo(element.href.replace(document.location.origin, ''))
+              return false
+            }
+        })
+    }
+
+    // Change to route defined by 'url' (if necessary)
+    init () {
+        let path = document.URL.replace(document.location.origin, ''),
+            cnt = 0,
+            routes = document.querySelectorAll(ObjRouteScreen.name)
+
+        window.removeEventListener('load', this.handler)
+
+        this.defaultRoute = '/' + routes[0].getAttribute('id')
+        for (cnt = 0; cnt < routes.length; cnt = cnt + 1) {
+            routes[cnt].style.display = 'none'
+        }
+
+        this.changeTo(path)
+    }
+
+    // Change route
+    async changeTo (path, fromNavigation) {
+        let hashPos = path.indexOf('#'),
+            cleanPath = path
+
+        // Set navigator URL
+        if (!fromNavigation) {
+            window.history.pushState( { html: path }, '', path)
+        }
+        
+        // Get real route
+        if (hashPos !== -1) {
+            cleanPath = path.substr(0, hashPos)
+        }
+
+        // Get 'home' path if necessary
+        if (cleanPath === '/') {
+            cleanPath = this.defaultRoute
+        }
+
+        // Show route changes
+        await this.showRoute(cleanPath)
+    }
+
+    // Hide old route and show the new one
+    async showRoute (path) {
+        let arr = path.split('/'),
+            refRoute = undefined
+
+        // Hide old route
+        refRoute = document.querySelector(ObjRouteScreen.name + '#' + this.route.substr(1))
+        if (refRoute.getAttribute('onhide')) {
+            await eval(refRoute.getAttribute('onhide'))
+        }
+        refRoute.style.display = 'none'
+        await this.waitNone(refRoute, true)
+
+        // Set scroll
+        document.body.scrollTop = 0
+
+        // Set and show new route
+        this.route = '/' + arr[1]
+        refRoute = document.querySelector(ObjRouteScreen.name + '#' + this.route.substr(1))
+        refRoute.style.display = ''
+        this.args = arr.splice(2)
+        await this.waitNone(refRoute, false)
+        if (refRoute.getAttribute('onshow')) {
+            await eval(refRoute.getAttribute('onshow'))
+        }
+    }
+
+    waitNone (ref, type) {
+        return new Promise(async (resolve, reject) => {
+            let style = window.getComputedStyle(ref),
+                now = style.getPropertyValue('display'),
+                condition = type ? now === 'none' : now !== 'none'
+            if (condition) {
+                resolve()
+            } else {
+                await this.promiseWait(1)
+                await this.waitNone(ref, type)
+            }
+        })
+    } 
+
+    promiseWait (time) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => { resolve() }, time)
+        })
+    }
+}
+
+var routing = new ObjRouting()
